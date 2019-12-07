47e41ca10770faee2f86be7eb1bd0c7cd508ae19
kasharma <kamal.sharma@netgear.com>
2019-12-07 20:21:22 +0530

    Code Setup Commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Login from './src/Authentication/login'
+
+class App extends Component {
+   render() {
+      return (
+         <div>
+            {/* <h1>Hello World</h1> */}
+            <Login/>
+         </div>
+      );
+   }
+}
+export default App;
---
+++ src/child1.js
@@ -0,0 +1,18 @@
+import React from 'react';
+export default class Child1 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            header: "Header from state...",
+            content: "Content from state..."
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.header}</h1>
+                <h2>{this.state.content}</h2>
+            </div>
+        );
+    }
+}
